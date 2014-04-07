a008bd7b6ead9380c5a812389f4eabfac60997e6
Alexander Standke <xanderstrike@gmail.com>
2014-04-07 15:55:00 -0700

    manage memory

--- avg.rb
+++ avg.rb
@@ -7,11 +7,13 @@ final = Image.new(1, 1)
 
 frames.each do |f|
   p f
-  img = Image.read("frames/#{f}").first.scale(1,1)
-  final = img.blend(final, 0.5)
+  img = Image.read("frames/#{f}").first
+  final = img.scale(1,1).blend(final, 0.5)
+  img.destroy! # don't fill up RAM oops
 end
 
 puts final.pixel_color(0,0)
+
 large = Image.new(512,512) {
   self.background_color = final.pixel_color(0, 0)
 }
